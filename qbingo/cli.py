6e555a23dd4e91440c66b4495128da4574967d2a
atilag <atilag@users.noreply.github.com>
2024-01-22 03:26:44 +0000

    ✅ Ready to clone and code.

--- qbingo/cli.py
+++ qbingo/cli.py
@@ -1,4 +1,4 @@
-"""CLI interface for project_name project.
+"""CLI interface for qbingo project.
 
 Be creative! do whatever you want!
 
@@ -12,7 +12,7 @@ Be creative! do whatever you want!
 def main():  # pragma: no cover
     """
     The main function executes on commands:
-    `python -m project_name` and `$ project_name `.
+    `python -m qbingo` and `$ qbingo `.
 
     This is your program's entry point.
 
